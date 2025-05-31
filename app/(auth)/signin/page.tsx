306803133902527532eed7ddb02c4c8c5fe6617d
MONU MANDAL <monumandel3@gmail.com>
2025-05-31 17:42:37 +0530

    setup env variables of vapi workflow and deploy app on vercel

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -2,7 +2,7 @@ import AuthForm from "@/components/AuthForm";
 import React from "react";
 
 const page = () => {
-  return <AuthForm type="sign-in" />;
+  return <AuthForm type="signin" />;
 };
 
 export default page;
--- constants/index.ts
+++ constants/index.ts
@@ -1,4 +1,4 @@
-// import { CreateAssistantDTO } from "@vapi-ai/web/dist/api";
+// import { CreateAssistantDTO } from "@vapi-ai/web/dist/api};
 import { z } from "zod";
 
 export const mappings = {
@@ -97,63 +97,6 @@ export const mappings = {
   "aws amplify": "amplify",
 };
 
-// export const interviewer: CreateAssistantDTO = {
-//   name: "Interviewer",
-//   firstMessage:
-//     "Hello! Thank you for taking the time to speak with me today. I'm excited to learn more about you and your experience.",
-//   transcriber: {
-//     provider: "deepgram",
-//     model: "nova-2",
-//     language: "en",
-//   },
-//   voice: {
-//     provider: "11labs",
-//     voiceId: "sarah",
-//     stability: 0.4,
-//     similarityBoost: 0.8,
-//     speed: 0.9,
-//     style: 0.5,
-//     useSpeakerBoost: true,
-//   },
-//   model: {
-//     provider: "openai",
-//     model: "gpt-4",
-//     messages: [
-//       {
-//         role: "system",
-//         content: `You are a professional job interviewer conducting a real-time voice interview with a candidate. Your goal is to assess their qualifications, motivation, and fit for the role.
-
-// Interview Guidelines:
-// Follow the structured question flow:
-// {{questions}}
-
-// Engage naturally & react appropriately:
-// Listen actively to responses and acknowledge them before moving forward.
-// Ask brief follow-up questions if a response is vague or requires more detail.
-// Keep the conversation flowing smoothly while maintaining control.
-// Be professional, yet warm and welcoming:
-
-// Use official yet friendly language.
-// Keep responses concise and to the point (like in a real voice interview).
-// Avoid robotic phrasing—sound natural and conversational.
-// Answer the candidate’s questions professionally:
-
-// If asked about the role, company, or expectations, provide a clear and relevant answer.
-// If unsure, redirect the candidate to HR for more details.
-
-// Conclude the interview properly:
-// Thank the candidate for their time.
-// Inform them that the company will reach out soon with feedback.
-// End the conversation on a polite and positive note.
-
-// - Be sure to be professional and polite.
-// - Keep all your responses short and simple. Use official language, but be kind and welcoming.
-// - This is a voice conversation, so keep your responses short, like in a real conversation. Don't ramble for too long.`,
-//       },
-//     ],
-//   },
-// };
-
 export const feedbackSchema = z.object({
   totalScore: z.number(),
   categoryScores: z.tuple([
@@ -239,3 +182,229 @@ export const dummyInterviews: Interview[] = [
     createdAt: "2024-03-14T15:30:00Z",
   },
 ];
+
+const monu_interview_prep = {
+  name: "monu_interview_prep",
+  nodes: [
+    {
+      name: "introduction",
+      type: "conversation",
+      isStart: true,
+      metadata: {
+        position: {
+          x: 86.91577202417619,
+          y: -503.3406768259967,
+        },
+      },
+      prompt: "Greet the user and help them create a new AI Interviewer",
+      model: {
+        model: "gpt-4o",
+        provider: "openai",
+        maxTokens: 1000,
+        temperature: 0.7,
+      },
+      voice: {
+        voiceId: "Neha",
+        provider: "vapi",
+      },
+      variableExtractionPlan: {
+        output: [
+          {
+            enum: [],
+            type: "string",
+            title: "level",
+            description: "The job experience level",
+          },
+          {
+            enum: [],
+            type: "string",
+            title: "amount",
+            description: "How many questions would you like to generate",
+          },
+          {
+            enum: [],
+            type: "string",
+            title: "techstack",
+            description:
+              "A list of technologies to cover during the job interview",
+          },
+          {
+            enum: [],
+            type: "string",
+            title: "role",
+            description:
+              "What role should would you like to train for? For example Frontend, Backend, Fullstack, Design, UX?",
+          },
+          {
+            enum: [],
+            type: "string",
+            title: "type",
+            description: "What type of the interview should it be?",
+          },
+        ],
+      },
+      messagePlan: {
+        firstMessage: "Hey there!",
+      },
+    },
+    {
+      name: "API Request",
+      type: "tool",
+      metadata: {
+        position: {
+          x: 40.60127494868783,
+          y: 231.79000508624028,
+        },
+      },
+      tool: {
+        url: `${process.env.NEXT_PUBLIC_BASE_URL}api/auth/vapi/generate`,
+        body: {
+          type: "object",
+          required: ["level", "amount", "techstack", "role", "type", "userid"],
+          properties: {
+            role: {
+              type: "string",
+              value: "{{ role }}",
+              description: "",
+            },
+            type: {
+              type: "string",
+              value: "{{ type }}",
+              description: "",
+            },
+            level: {
+              type: "string",
+              value: "{{ level }}",
+              description: "",
+            },
+            amount: {
+              type: "string",
+              value: "{{ amount }}",
+              description: "",
+            },
+            userid: {
+              type: "string",
+              value: "{{userid}}",
+              description: "",
+            },
+            techstack: {
+              type: "string",
+              value: "{{ techstack }}",
+              description: "",
+            },
+          },
+        },
+        type: "apiRequest",
+        method: "POST",
+        function: {
+          name: "untitled_tool",
+          parameters: {
+            type: "object",
+            required: [],
+            properties: {},
+          },
+        },
+      },
+    },
+    {
+      name: "conversation_1748620587097",
+      type: "conversation",
+      metadata: {
+        position: {
+          x: 59.8268935329111,
+          y: -93.19555125863187,
+        },
+      },
+      prompt: "Say that the Interview will be generated shortly",
+      model: {
+        model: "gpt-4o",
+        provider: "openai",
+        maxTokens: 1000,
+        temperature: 0.7,
+      },
+      voice: {
+        voiceId: "Neha",
+        provider: "vapi",
+      },
+      messagePlan: {
+        firstMessage: "",
+      },
+    },
+    {
+      name: "conversation_1748620832903",
+      type: "conversation",
+      metadata: {
+        position: {
+          x: 55.13607411358862,
+          y: 554.4640009107442,
+        },
+      },
+      prompt:
+        " thank the user for their time and informing them that the interview has been generated successfully",
+      model: {
+        model: "gpt-4o",
+        provider: "openai",
+        maxTokens: 1000,
+        temperature: 0.7,
+      },
+      voice: {
+        voiceId: "Neha",
+        provider: "vapi",
+      },
+      messagePlan: {
+        firstMessage: "",
+      },
+    },
+    {
+      name: "hangup_1748620864566",
+      type: "tool",
+      metadata: {
+        position: {
+          x: 70.82837849785952,
+          y: 964.3467917246528,
+        },
+      },
+      tool: {
+        type: "endCall",
+      },
+    },
+  ],
+  edges: [
+    {
+      from: "introduction",
+      to: "conversation_1748620587097",
+      condition: {
+        type: "ai",
+        prompt: "If user provided all the required variables",
+      },
+    },
+    {
+      from: "conversation_1748620587097",
+      to: "API Request",
+      condition: {
+        type: "ai",
+        prompt: "user said yes",
+      },
+    },
+    {
+      from: "API Request",
+      to: "conversation_1748620832903",
+      condition: {
+        type: "ai",
+        prompt: "user said yes",
+      },
+    },
+    {
+      from: "conversation_1748620832903",
+      to: "hangup_1748620864566",
+      condition: {
+        type: "ai",
+        prompt: "user said yes",
+      },
+    },
+  ],
+  globalPrompt:
+    "You are a voice assistant helping with creating new AI interviewers. Your task is to collect data from the user. Remember that this is a voice conversation - do not use any special characters.",
+};
+
+export default monu_interview_prep;
